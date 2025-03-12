36b5a615aafe9c069df3f8bc76f28ee4253854fe
Dhruv <dhruvdm02@gmail.com>
2025-03-12 22:44:22 +0530

    Service Layer

--- src/main/java/com/h3ydhruv/springbootwebtutorial/springbootwebtutorial/entities/EmployeeEntity.java
+++ src/main/java/com/h3ydhruv/springbootwebtutorial/springbootwebtutorial/entities/EmployeeEntity.java
@@ -1,10 +1,7 @@
 package com.h3ydhruv.springbootwebtutorial.springbootwebtutorial.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -17,13 +14,11 @@ import java.time.LocalDate;
 public class EmployeeEntity {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_seq_gen")
-    @SequenceGenerator(name = "employee_seq_gen", sequenceName = "EMPLOYEES_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String email;
     private Integer age;
     private LocalDate dateOfJoining;
     private Boolean isActive;
-
 }
